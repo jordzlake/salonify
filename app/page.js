5365d9d28c3cc862d7b037c5069f69eb28e6c42a
jordzlake <jordanlake18@outlook.com>
2026-02-06 23:58:11 -0400

    webimgs

--- app/page.js
+++ app/page.js
@@ -1,5 +1,16 @@
 import MapView from "../components/MapView";
 
+export const metadata = {
+  title: "Salonify",
+  description: "Find the best salons near you with Salonify! I love pookies!",
+  openGraph: {
+    title: "Salonify",
+    description: "Find the best salons near you with Salonify! I love pookies!",
+    images: ["/imgs/logo.png"], // ✅ put it in public folder
+    type: "website",
+  },
+};
+
 export default function Page() {
   return (
     <main style={{ padding: "2rem" }}>
